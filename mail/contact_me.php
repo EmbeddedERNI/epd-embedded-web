c7a9eb4458f096fb06380c2c78dc124adb072a32
Hernan Bartoletti <hernan.bartoletti@gmail.com>
2017-11-01 22:02:19 +0000

    Modify contact files

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,22 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['rate'])      ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_POST['rate'])      ||
+   empty($_POST['message'])   )
    {
    echo "No arguments Provided!";
    return false;
    }
    
-$name = strip_tags(htmlspecialchars($_POST['name']));
+$name = empty($_POST['name']) ? 'anonymous' : strip_tags(htmlspecialchars($_POST['name']));
 $rate = strip_tags(htmlspecialchars($_POST['rate']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'pi@localhost'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nRate: $rate\n\nMessage:\n$message";
+$to = 'hernan@localhost'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Feedback:  $name";
+$email_body = "You have received a Feedback.\n\n"."Here are the details:\n\nName: $name\n\nRate: $rate\n\nMessage:\n$message";
 $headers = "From: noreply@localhost\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers .= "Reply-To: noreply@localhost";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
